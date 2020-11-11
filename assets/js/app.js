d9673f86370104876a2818673d4d5eac483f87f7
giwajossy <giwajosef@gmail.com>
2020-11-11 07:15:40 +0100

    Refactor dynamic browser theme-color

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,27 @@
+//Function to convert hex format to a rgb color
+function rgb2hex(rgb){
+    rgb = rgb.match(/^[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+    return (rgb && rgb.length === 4) ? "#" +
+     ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+     ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+     ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
+
+
+
 function colorChange() {
     setInterval(function(){ 
         let red = Math.floor(Math.random() * 255);
         let green = Math.floor(Math.random() * 255);
         let blue = Math.floor(Math.random() * 255);
-        let alpha = Math.random();
+        // let alpha = Math.random();
+        let alpha = 1;
         // document.getElementById("body").style.backgroundColor = "rgba(192,57,43,1)";
         // document.querySelector(".kits button").style.borderBottom = "2em solid rgba("+red+","+green+","+blue+","+alpha+")"; 
         document.getElementById("body").style.backgroundColor = "rgba("+red+","+green+","+blue+","+alpha+")"; 
-        document.getElementsByTagName("Meta")[4].setAttribute("content", "rgba("+red+","+green+","+blue+","+alpha+")");
+        var package = red+","+green+","+blue;
+        document.getElementsByTagName("Meta")[4].setAttribute("content", rgb2hex(package));
+        // document.getElementsByTagName("Meta")[4].setAttribute("content", "rgba("+red+","+green+","+blue+","+alpha+")");
     }, 5000);
 }
 
