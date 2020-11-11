41ea62faff08290a3ffe17339a6256725c679847
giwajossy <giwajosef@gmail.com>
2020-11-11 07:01:02 +0100

    Handle buttonClick // Activate dynamic browser theme-color

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,8 +7,21 @@ function colorChange() {
         // document.getElementById("body").style.backgroundColor = "rgba(192,57,43,1)";
         // document.querySelector(".kits button").style.borderBottom = "2em solid rgba("+red+","+green+","+blue+","+alpha+")"; 
         document.getElementById("body").style.backgroundColor = "rgba("+red+","+green+","+blue+","+alpha+")"; 
+        document.getElementsByTagName("Meta")[4].setAttribute("content", "rgba("+red+","+green+","+blue+","+alpha+")");
     }, 5000);
 }
 
 
 
+var numberOfButtons = document.querySelectorAll(".instrument").length;
+
+for (var i = 0; i<numberOfButtons; i++) {
+    document.querySelectorAll(".instrument")[i].addEventListener("click", buttonClicked);
+
+    function buttonClicked() {
+        var buttonContent = document.querySelectorAll(".instrument")[i].innerText;
+        alert(buttonContent);
+        // alert("I got here");
+    
+    }
+}
